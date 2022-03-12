47ebcadca084d764e73af368e3497fc951c96aed
Thong <chhanbunthong@gmail.com>
2022-03-12 15:51:05 +0700

    [Init] - setup project with express

---
+++ app.js
@@ -0,0 +1,5 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+
+app.listen(process.env.PORT, () => console.log(`Server is running on port ${process.env.PORT}`))
