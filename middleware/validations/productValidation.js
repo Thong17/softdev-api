d7ba113a5798184d5e2269667ed4f985466805fc
SATHAPANA\chhan.bunthong <chhan.bunthong@sathapana.com.kh>
2022-06-08 09:23:30 +0700

    [update] - change product model

--- middleware/validations/productValidation.js
+++ middleware/validations/productValidation.js
@@ -2,9 +2,18 @@ const Joi = require('joi')
 
 const createProductValidation = Joi.object({
     name: Joi.object().required(),
+    price: Joi.number().required(),
+    currency: Joi.string().required(),
+    code: Joi.string().optional().allow(''),
     status: Joi.boolean().optional(),
-    icon: Joi.any().optional(),
-    description: Joi.string().optional().allow('')
+    description: Joi.string().optional().allow(''),
+    isStock: Joi.boolean().optional(),
+    profile: Joi.any().optional(),
+    brand: Joi.string().required(),
+    category: Joi.string().required(),
+    colors: Joi.array().optional(),
+    options: Joi.array().optional(),
+    images: Joi.array().optional()
 })
 
 module.exports = {
