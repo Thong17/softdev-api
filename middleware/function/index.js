a26ae4b1d8c44d3152c1272bb002bb6b88967c80
SATHAPANA\chhan.bunthong <chhan.bunthong@sathapana.com.kh>
2022-11-26 22:37:39 +0700

    [update] - clear all payment

---
+++ middleware/function/index.js
@@ -0,0 +1,22 @@
+const { reverseProductStock } = require('../../helpers/utils')
+const Transaction = require('../../models/Transaction')
+
+exports.clearPendingTransaction = () => {
+    return async (req, res, next) => {
+        try {
+            const pendingTransaction = await Transaction.find({ status: false, isDeleted: false })
+            if (pendingTransaction.length > 0) {
+                pendingTransaction.forEach(transaction => {
+                    reverseProductStock(transaction?.stocks)
+                        .then(async () => {
+                            await Transaction.findByIdAndDelete(transaction._id)
+                        })
+                        .catch(err => console.error(err))
+                })
+            }
+            next()
+        } catch (err) {
+            console.error(err)
+        }
+    }
+}
