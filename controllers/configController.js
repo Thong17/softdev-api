6928079a49c0e2426c5337eb68e316b7a3f166ee
Thong17 <chhanbunthong@gmail.com>
2026-05-21 20:23:36 +0700

    update: config cert

--- controllers/configController.js
+++ controllers/configController.js
@@ -1,9 +1,13 @@
 const jsrsasign = require('jsrsasign');
 const fs = require("node:fs")
 
-const privateKey = fs.readFileSync("static/private/private-key.pem", "utf-8");
+const privateKey = fs.readFileSync(process.env.QZ_TRAY_PRIVATE_KEY_PATH, "utf-8");
+const certificate = fs.readFileSync(process.env.QZ_TRAY_CERTIFICATE_PATH, "utf-8");
 
 exports.signQzCert = async (req, res) => {
+    if (!req.body.data || !privateKey) {
+        return res.status(400).json({ error: "Missing data or private key" });
+    }
     const pk = jsrsasign.KEYUTIL.getKey(privateKey);
     const sig = new jsrsasign.KJUR.crypto.Signature({"alg": "SHA512withRSA"});  // Use "SHA1withRSA" for QZ Tray 2.0 and older
     sig.init(pk); 
@@ -12,3 +16,9 @@ exports.signQzCert = async (req, res) => {
     res.json({ signature: jsrsasign.stob64(jsrsasign.hextorstr(hex)) });
 }
 
+exports.getCertificate = async (req, res) => {
+    if (!certificate) {
+        return res.status(400).json({ error: "Missing certificate in environment variables" });
+    }
+    res.json({ certificate });
+}
--- routes/config/index.js
+++ routes/config/index.js
@@ -1,8 +1,12 @@
 const router = require('express').Router()
-const { signQzCert } = require('../../controllers/configController')
+const { signQzCert, getCertificate } = require('../../controllers/configController')
 
 router.post('/sign-qz-cert', (req, res) => {
     signQzCert(req, res)
+})
+
+router.post('/get-certificate', (req, res) => {
+    getCertificate(req, res)
 })
 
 module.exports = router
